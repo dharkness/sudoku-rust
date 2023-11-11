0e3e79358290621a816116a9a8c26b77c247348f
David Harkness <dharkness@gmail.com>
2023-11-10 22:37:06 -0800

    Add test for XYZ-Wing solver

--- src/solve/algorithms/xyz_wings.rs
+++ src/solve/algorithms/xyz_wings.rs
@@ -25,13 +25,14 @@ pub fn find_xyz_wings(board: &Board) -> Option<Effects> {
 
     tri_values.iter().for_each(|pivot| {
         // let (k1, k2) = board.candidates(cell).as_pair().unwrap();
-        (pivot.peers() & bi_values)
+        let pivot_peers = pivot.peers();
+        (pivot_peers & bi_values)
             .iter()
             .combinations(2)
             .map(|pair| pair.iter().copied().union_cells())
             .for_each(|pair| {
                 let (c1, c2) = pair.as_pair().expect("cell pair");
-                let candidates = pivot.peers() & c1.peers() & c2.peers();
+                let candidates = pivot_peers & c1.peers() & c2.peers();
                 if candidates.len() != 2 {
                     // degenerate naked triple
                     return;
@@ -68,5 +69,39 @@ pub fn find_xyz_wings(board: &Board) -> Option<Effects> {
         Some(effects)
     } else {
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::io::{Parse, Parser};
+    use crate::layout::cells::cell::cell;
+    use crate::layout::cells::cell_set::cells;
+    use crate::layout::values::known::known;
+
+    use super::*;
+
+    #[test]
+    fn all_in_pivot() {
+        let parser = Parse::wiki().stop_on_error();
+        let (board, effects, failed) = parser.parse(
+            "88g1052g8g0341112o110q6i054gm02og2812i8i6ijg09v005g22g8841110a860hg1210605880iga21o08i41110i21g14116948i090m2i1i813ig1703a056a411m2i3i1m093281g1g11609815674320h62",
+        );
+        assert_eq!(None, failed);
+        assert!(!effects.has_errors());
+
+        if let Some(got) = find_xyz_wings(&board) {
+            assert_eq!(1, got.actions().len());
+
+            let mut action = Action::new(Strategy::XYZWing);
+            action.erase(cell!("F7"), known!("1"));
+            action.clue_cells_for_known(Verdict::Secondary, cells!("D9 F1 F9"), known!("1"));
+            action.clue_cells_for_known(Verdict::Primary, cells!("D9 F9"), known!("2"));
+            action.clue_cells_for_known(Verdict::Primary, cells!("F1 F9"), known!("4"));
+
+            assert_eq!(format!("{:?}", action), format!("{:?}", got.actions()[0]));
+        } else {
+            panic!("not found");
+        }
     }
 }
