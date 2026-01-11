97893cc4cdea584397f24e6c59e48815323ea6bf
David Harkness <dharkness@gmail.com>
2026-01-11 12:25:25 -0800

    Remove unused Deref, DerefMut, and From traits from Bit

--- src/layout/cells/bit.rs
+++ src/layout/cells/bit.rs
@@ -1,6 +1,3 @@
-use std::ops::{Deref, DerefMut};
-
-use super::label::index_from_label;
 use super::Cell;
 
 /// Specifies a single cell by its position in a bit field.
@@ -26,31 +23,5 @@ impl Bit {
 
     pub const fn cell(&self) -> Cell {
         Cell::new(self.index())
-    }
-}
-
-impl Deref for Bit {
-    type Target = u128;
-
-    fn deref(&self) -> &u128 {
-        &self.0
-    }
-}
-
-impl DerefMut for Bit {
-    fn deref_mut(&mut self) -> &mut u128 {
-        &mut self.0
-    }
-}
-
-impl From<&str> for Bit {
-    fn from(label: &str) -> Self {
-        Self(1 << index_from_label(label))
-    }
-}
-
-impl From<Cell> for Bit {
-    fn from(cell: Cell) -> Self {
-        cell.bit()
     }
 }
